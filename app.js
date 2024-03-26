0b633e80944ecccb1ededcb19ae3913ef39d942e
YudaDh <yudadarmawan26@gmail.com>
2024-03-27 00:44:52 +0800

    Fetch marker data from database and show popup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
-import fs from "fs";
+// import fs from "fs";
+import db from "./database.js";
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,21 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/markers", async (req, res) => {
+  try {
+    const connection = await db.getConnection();
+    console.log("Database Connected");
+    const [rows, fields] = await connection.query("SELECT * FROM hospital");
+    res.status(200).json({
+      data: rows,
+      message: "Success fetching markers data",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error fetching markers data" });
+  }
 });
 
 // app.post("/simpan", (req, res) => {
@@ -33,45 +49,45 @@ app.get("/", (req, res) => {
 //   });
 // });
 
-app.post("/simpan", (req, res) => {
-  const koordinat = req.body; // Assuming 'koordinat' is sent in the request body
-  // console.log(koordinat);
-  // Write data to a JSON file named 'data.json'
-  fs.writeFile("data.txt", JSON.stringify(koordinat), (err) => {
-    if (err) {
-      console.error("Error writing to file:", err);
-      res.status(500).json({ message: "Error saving data" });
-    } else {
-      console.log("Data saved successfully");
-      res.status(200).json({ message: "Data saved successfully" });
-    }
-  });
-});
+// app.post("/simpan", (req, res) => {
+//   const koordinat = req.body; // Assuming 'koordinat' is sent in the request body
+//   // console.log(koordinat);
+//   // Write data to a JSON file named 'data.json'
+//   fs.writeFile("data.txt", JSON.stringify(koordinat), (err) => {
+//     if (err) {
+//       console.error("Error writing to file:", err);
+//       res.status(500).json({ message: "Error saving data" });
+//     } else {
+//       console.log("Data saved successfully");
+//       res.status(200).json({ message: "Data saved successfully" });
+//     }
+//   });
+// });
 
-//Define a route to handle the GET request
-app.get("/baca", (req, res) => {
-  // Read data from the 'data.json' file
-  fs.readFile("data.txt", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading file:", err);
-      res.status(500).json({ message: "Error reading data" });
-    } else {
-      const jsonData = JSON.parse(data);
-      res.status(200).json(jsonData);
-    }
-  });
-});
-//Delete Data
-app.delete("/hapus", (req, res) => {
-  fs.writeFile("data.txt", "[]", (err) => {
-    if (err) {
-      res.status(500).json({ message: "Error deleting data" });
-    } else {
-      res.status(200).json({ message: "Data deleted successfully" });
-    }
-  });
-});
+// //Define a route to handle the GET request
+// app.get("/baca", (req, res) => {
+//   // Read data from the 'data.json' file
+//   fs.readFile("data.txt", "utf8", (err, data) => {
+//     if (err) {
+//       console.error("Error reading file:", err);
+//       res.status(500).json({ message: "Error reading data" });
+//     } else {
+//       const jsonData = JSON.parse(data);
+//       res.status(200).json(jsonData);
+//     }
+//   });
+// });
+// //Delete Data
+// app.delete("/hapus", (req, res) => {
+//   fs.writeFile("data.txt", "[]", (err) => {
+//     if (err) {
+//       res.status(500).json({ message: "Error deleting data" });
+//     } else {
+//       res.status(200).json({ message: "Data deleted successfully" });
+//     }
+//   });
+// });
 
 app.listen(port, () => {
-  console.log(`Server start at ${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   // popup box html
   //   const popupBox = (lat, long, index) => {
   //     return ` <div class="text-slate-900 dark:text-white">
@@ -30,128 +30,171 @@ document.addEventListener("DOMContentLoaded", function () {
     iconSize: [40, 40],
   });
   // marker
+  async function getMarkers() {
+    try {
+      const res = await axios.get("/markers");
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  try {
+    const getMarkerData = await getMarkers();
+    if (getMarkerData && getMarkerData.data) {
+      const markerArray = getMarkerData.data;
+      markerArray.forEach((marker) => {
+        const mark = L.marker([marker.longitude, marker.latitude]).addTo(map);
+        // Create a div element for the custom popup content
+        var popupContent = document.createElement("div");
+        popupContent.classList.add("custom-popup");
+
+        const markerContent = document.createElement("div");
+        markerContent.innerHTML = `
+                  <h2>Marker Details</h2>
+                  <p>Name: ${marker.name}</p>
+                  <p>Latitude: ${marker.latitude}</p>
+                  <p>Longitude: ${marker.longitude}</p>
+                  <p>Regency: ${marker.regency}</p>
+                  <!-- Add more details as needed -->
+                  `;
+        // <span class="close-button" onclick="closePopup()">Close</span>
+        popupContent.appendChild(markerContent);
+        mark.bindPopup(popupContent);
+        mark.on("click", function () {
+          mark.openPopup();
+        });
+      });
+    } else {
+      console.error("Invalid marker data received:", getMarkerData);
+    }
+  } catch (error) {
+    console.error("Error fetching marker data:", error);
+  }
+
   // let marker = L.marker([-8.4095188, 115.188919], {
   //   icon: myIcon,
   // }).addTo(map);
   // circle
-  let circle = L.circle([-8.654956, 115.205618], {
-    color: "red",
-    fillcolor: "#00000",
-    fillopacity: 0.5,
-    radius: 1000,
-  }).addTo(map);
-  let markers = [];
-  let markerClusters = L.markerClusterGroup().addTo(map);
-  addMarker = function (latlng, index) {
-    // menambahkan marker
-    let myMarker = L.marker(latlng, {
-      icon: myIcon,
-      draggable: true,
-    }).addTo(map);
+  // let circle = L.circle([-8.654956, 115.205618], {
+  //   color: "red",
+  //   fillcolor: "#00000",
+  //   fillopacity: 0.5,
+  //   radius: 1000,
+  // }).addTo(map);
+  // let markers = [];
+  // let markerClusters = L.markerClusterGroup().addTo(map);
+  // addMarker = function (latlng, index) {
+  //   // menambahkan marker
+  //   let myMarker = L.marker(latlng, {
+  //     icon: myIcon,
+  //     draggable: true,
+  //   }).addTo(map);
 
-    let thisPopup = L.popup();
-    myMarker.bindPopup(thisPopup);
+  //   let thisPopup = L.popup();
+  //   myMarker.bindPopup(thisPopup);
 
-    myMarker.on("click", function () {
-      thisPopup.setLatLng(myMarker.getLatLng());
-      thisPopup.setContent(`
-      <table>
-        <tr>
-          <td>Latitude</td><td>:</td><td>${myMarker.getLatLng().lat}</td>
-        </tr>
-        <tr>
-          <td>Latitude</td><td>:</td><td>${myMarker.getLatLng().lng}</td>
-        </tr>
-        <tr>
-          <td>MarkerNum</td><td>:</td><td>${index + 1}</td>
-        </tr>
-      </table>
-      `);
-      //   thisPopup.setContent(
-      //     popupBox(myMarker.getLatLng().lat, myMarker.getLatLng().lng, index)
-      //   );
-      thisPopup.addTo(map);
-    });
-    // delete marker
-    myMarker.on("contextmenu", function (e) {
-      markers.forEach((marker, index) => {
-        if (myMarker == marker) {
-          myMarker.removeFrom(map);
-          markers.splice(index, 1);
-        }
-      });
-    });
-    // console.log(markers);
-    return myMarker;
-  };
-  // add marker
-  map.on("click", function (e) {
-    // console.log(e);
-    let newMarker = addMarker(e.latlng, markers.length);
-    markers.push(newMarker);
-    markerClusters.addLayer(L.layerGroup(markers));
-  });
-  var btnKirim = document.getElementById("btnKirim");
-  btnKirim.addEventListener("click", function () {
-    // Ambil koordinat masing-masing marker dan simpan ke dalam array koordinat
-    var koordinat = markers.map(function (marker) {
-      return [marker.getLatLng().lat, marker.getLatLng().lng];
-    });
-    // console.log(JSON.stringify(koordinat));
-    // Kirim data ke server dalam format JSON menggunakan method fetch()
-    fetch("/simpan", {
-      method: "POST",
-      body: JSON.stringify(koordinat),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then(function (response) {
-        return response;
-      })
-      .then(function (data) {
-        console.log(data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  });
+  //   myMarker.on("click", function () {
+  //     thisPopup.setLatLng(myMarker.getLatLng());
+  //     thisPopup.setContent(`
+  //     <table>
+  //       <tr>
+  //         <td>Latitude</td><td>:</td><td>${myMarker.getLatLng().lat}</td>
+  //       </tr>
+  //       <tr>
+  //         <td>Latitude</td><td>:</td><td>${myMarker.getLatLng().lng}</td>
+  //       </tr>
+  //       <tr>
+  //         <td>MarkerNum</td><td>:</td><td>${index + 1}</td>
+  //       </tr>
+  //     </table>
+  //     `);
+  //     //   thisPopup.setContent(
+  //     //     popupBox(myMarker.getLatLng().lat, myMarker.getLatLng().lng, index)
+  //     //   );
+  //     thisPopup.addTo(map);
+  //   });
+  //   // delete marker
+  //   myMarker.on("contextmenu", function (e) {
+  //     markers.forEach((marker, index) => {
+  //       if (myMarker == marker) {
+  //         myMarker.removeFrom(map);
+  //         markers.splice(index, 1);
+  //       }
+  //     });
+  //   });
+  //   // console.log(markers);
+  //   return myMarker;
+  // };
+  // // add marker
+  // map.on("click", function (e) {
+  //   // console.log(e);
+  //   let newMarker = addMarker(e.latlng, markers.length);
+  //   markers.push(newMarker);
+  //   markerClusters.addLayer(L.layerGroup(markers));
+  // });
+  // var btnKirim = document.getElementById("btnKirim");
+  // btnKirim.addEventListener("click", function () {
+  //   // Ambil koordinat masing-masing marker dan simpan ke dalam array koordinat
+  //   var koordinat = markers.map(function (marker) {
+  //     return [marker.getLatLng().lat, marker.getLatLng().lng];
+  //   });
+  //   // console.log(JSON.stringify(koordinat));
+  //   // Kirim data ke server dalam format JSON menggunakan method fetch()
+  //   fetch("/simpan", {
+  //     method: "POST",
+  //     body: JSON.stringify(koordinat),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then(function (response) {
+  //       return response;
+  //     })
+  //     .then(function (data) {
+  //       console.log(data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // });
 
-  fetch("/baca")
-    .then(function (response) {
-      // return response.text();
-      return response.json();
-    })
-    .then(function (data) {
-      var latlangs = [];
-      data.forEach(function (c, i) {
-        let latlng = L.latLng(c[0], c[1]);
-        latlangs.push(latlng);
-        var newMarker = addMarker(latlng, markers.length);
-        markers.push(newMarker);
-      });
+  // fetch("/baca")
+  //   .then(function (response) {
+  //     // return response.text();
+  //     return response.json();
+  //   })
+  //   .then(function (data) {
+  //     var latlangs = [];
+  //     data.forEach(function (c, i) {
+  //       let latlng = L.latLng(c[0], c[1]);
+  //       latlangs.push(latlng);
+  //       var newMarker = addMarker(latlng, markers.length);
+  //       markers.push(newMarker);
+  //     });
 
-      // Tambahkan ke markercluster
-      markerClusters.addLayer(L.layerGroup(markers));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  //     // Tambahkan ke markercluster
+  //     markerClusters.addLayer(L.layerGroup(markers));
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 
-  const btnHapus = document.getElementById("btnHapus");
-  btnHapus.addEventListener("click", function () {
-    fetch("/hapus", {
-      method: "DELETE",
-    })
-      .then(function (res) {
-        return res.json();
-      })
-      .then((data) => {
-        alert(data.message);
-        window.location.reload();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+  // const btnHapus = document.getElementById("btnHapus");
+  // btnHapus.addEventListener("click", function () {
+  //   fetch("/hapus", {
+  //     method: "DELETE",
+  //   })
+  //     .then(function (res) {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       alert(data.message);
+  //       window.location.reload();
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // });
 });
